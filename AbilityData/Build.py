e036355685db4fdd6e8b043d89cf9969fa70f02a
SabreValkyrn <61754281+SabreValkyrn@users.noreply.github.com>
2021-12-24 23:03:56 -0800

    Overhaul ability data and processing (#13)
    * Remove old DataTables
    
    * Add reverse spellID lookups
    
    * Rebuild AbilityData
    
    * Convert combat parsing to new data

---
+++ AbilityData/Build.py
@@ -0,0 +1,71 @@
+# 1. Open https://docs.google.com/spreadsheets/d/1jtx1WyfChzACzh0WBWANtrqkRtS3D-zPWqs3eOnyVvY/edit#gid=0
+# 2. Download as CSV to ".\AbilityData\bcc.csv"
+# 2. Execute this script from root, "python .\AbilityData\Build.py"
+
+from datetime import date
+
+import csv, os
+
+if not os.path.isdir('./AbilityData'):
+  print('Must be run from Addon root')
+  exit()
+
+header = []
+ability_list = []
+reverse_lookup = {}
+
+with open('./AbilityData/bcc.csv', 'r', newline='') as csvfile:
+  csvreader = csv.DictReader(csvfile)
+  
+  headers = next(csvreader)
+  print('Parsing file with: ' + ', '.join(headers))
+
+  excluded_count = 0
+  ability_group_lookup = {}
+
+  for ability in csvreader:
+    parsed_rank = ability['Rank'] if ability['Rank'] else 1
+
+    if ability['Include'] != 'No':
+      ability_group_name = '{0} - {1}'.format(ability['Class'], ability['Ability'])
+
+      if not ability_group_name in ability_group_lookup:
+        ability_group_lookup[ability_group_name] = {}
+
+      ability_group_id = len(ability_group_lookup)
+
+      ability_group_lookup[ability_group_name] = ability_group_id
+
+      ability_list.append(
+        "  [{0}] = {{ Rank = {1}, Level = {2}, AbilityGroup = {3} }},\n"
+          .format(ability['Ability ID'], parsed_rank, ability['Level'], ability_group_id)
+        )
+
+      if not ability_group_id in reverse_lookup:
+        reverse_lookup[ability_group_id] = {}
+
+      reverse_lookup[ability_group_id][ability['Ability ID']] = parsed_rank
+
+    else:
+      print('Excluding {0}{1} - {2}'.format(ability['Ability'], parsed_rank, ability['Note']))
+      excluded_count += 1
+
+  print('{0} Loaded / {1} Excluded abilities'.format(len(ability_list), excluded_count))
+
+
+
+with open('./AbilityData.lua', 'w', newline='') as abilityData:
+  abilityData.write('-- Built on {0}\n\n'.format(date.today()))
+  abilityData.write('SpellSentinel.BCC = { }\n\n')
+  abilityData.write('SpellSentinel.BCC.AbilityData = {\n')
+
+  for ability in ability_list:
+    abilityData.write(ability);
+
+  abilityData.write('}\n\n')
+
+  abilityData.write('SpellSentinel.BCC.AbilityGroups = {\n')
+  for key, abilityGroup in reverse_lookup.items():
+    abilityData.write('  {{ {1} }},\n'.format(key, ', '.join(abilityGroup) ));
+
+  abilityData.write('}\n')
--- Cache.lua
+++ Cache.lua
@@ -13,11 +13,13 @@ end
 function SpellSentinel:ClearCache()
     local count = self:CountCache(self.db.profile.announcedSpells);
     local playerCount = self:CountCache(self.db.profile.ignoredPlayers);
+    local isMaxRankCount = self:CountCache(self.db.profile.isMaxRank);
 
     self.db.profile.announcedSpells = {};
     self.db.profile.ignoredPlayers = {};
+    self.db.profile.isMaxRank = {};
 
     self:PrintMessage(string.format(
-                          "Cache reset: %d entries purged and %d players unignored",
-                          count, playerCount));
+                          "Cache reset: %d entries purged, %d players unignored, and %d cached results",
+                          count, playerCount, isMaxRankCount));
 end
