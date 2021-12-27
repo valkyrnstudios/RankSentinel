51e1c2b9731618e5ae428ec4e2da5049563e86cb
SabreValkyrn <sabre@valkyrnstudios.net>
2021-12-27 12:42:48 -0800

    Update Build.py for multiple-eras

--- AbilityData/Build.py
+++ AbilityData/Build.py
@@ -11,62 +11,70 @@ if not os.path.isdir('./AbilityData'):
   print('Must be run from Addon root')
   exit()
 
-header = []
-ability_list = []
-reverse_lookup = {}
+eras = {
+  'TBC.lua': 'tbc.csv',
+  'Vanilla.lua': 'vanilla.csv'
+}
 
-with open('./AbilityData/tbc.csv', 'r', newline='') as csvfile:
-  csvreader = csv.DictReader(csvfile)
-  
-  headers = next(csvreader)
-  print('Parsing file with: ' + ', '.join(headers))
+for eraLua, eraCsv in eras.items():
+  if not os.path.isdir('./AbilityData'):
+    print('Missing {0}'.format(eraCsv));
+    exit()
 
-  excluded_count = 0
-  ability_group_lookup = {}
+  header = []
+  ability_list = []
+  reverse_lookup = {}
 
-  for ability in csvreader:
-    parsed_rank = ability['Rank'] if ability['Rank'] else 1
+  with open('./AbilityData/{0}'.format(eraCsv), 'r', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile)
 
-    if ability['Include'] != 'No':
-      ability_group_name = '{0} - {1}'.format(ability['Class'], ability['Ability'])
+    headers = next(csvreader)
+    print('Parsing file with: ' + ', '.join(headers))
 
-      if not ability_group_name in ability_group_lookup:
-        ability_group_lookup[ability_group_name] = {}
+    excluded_count = 0
+    ability_group_lookup = {}
 
-      ability_group_id = len(ability_group_lookup)
+    for ability in csvreader:
+      parsed_rank = ability['Rank'] if ability['Rank'] else 1
 
-      ability_group_lookup[ability_group_name] = ability_group_id
+      if ability['Include'] != 'No':
+        ability_group_name = '{0} - {1}'.format(ability['Class'], ability['Ability'])
 
-      ability_list.append(
-        "  [{0}] = {{ Rank = {1}, Level = {2}, AbilityGroup = {3} }},\n"
-          .format(ability['Ability ID'], parsed_rank, ability['Level'], ability_group_id)
-        )
+        if not ability_group_name in ability_group_lookup:
+          ability_group_lookup[ability_group_name] = {}
 
-      if not ability_group_id in reverse_lookup:
-        reverse_lookup[ability_group_id] = {}
+        ability_group_id = len(ability_group_lookup)
 
-      reverse_lookup[ability_group_id][ability['Ability ID']] = parsed_rank
+        ability_group_lookup[ability_group_name] = ability_group_id
 
-    else:
-      print('Excluding {0}{1} - {2}'.format(ability['Ability'], parsed_rank, ability['Note']))
-      excluded_count += 1
+        ability_list.append(
+          "  [{0}] = {{ Rank = {1}, Level = {2}, AbilityGroup = {3} }},\n"
+            .format(ability['Ability ID'], parsed_rank, ability['Level'], ability_group_id)
+          )
 
-  print('{0} Loaded / {1} Excluded abilities'.format(len(ability_list), excluded_count))
+        if not ability_group_id in reverse_lookup:
+          reverse_lookup[ability_group_id] = {}
 
+        reverse_lookup[ability_group_id][ability['Ability ID']] = parsed_rank
 
+      else:
+        print('Excluding {0}{1} - {2}'.format(ability['Ability'], parsed_rank, ability['Note']))
+        excluded_count += 1
 
-with open('./AbilityData/TBC.lua', 'w', newline='') as abilityData:
-  abilityData.write('-- Built on {0}\n\n'.format(date.today()))
-  abilityData.write('local _, addon = ...\n\n')
-  abilityData.write('addon.AbilityData = {\n')
+    print('{0} Loaded / {1} Excluded abilities'.format(len(ability_list), excluded_count))
 
-  for ability in ability_list:
-    abilityData.write(ability);
+  with open('./AbilityData/{0}'.format(eraLua), 'w', newline='') as abilityData:
+    abilityData.write('-- Built on {0}\n\n'.format(date.today()))
+    abilityData.write('local _, addon = ...\n\n')
+    abilityData.write('addon.AbilityData = {\n')
 
-  abilityData.write('}\n\n')
+    for ability in ability_list:
+      abilityData.write(ability);
 
-  abilityData.write('addon.AbilityGroups = {\n')
-  for key, abilityGroup in reverse_lookup.items():
-    abilityData.write('  {{ {1} }},\n'.format(key, ', '.join(abilityGroup) ));
+    abilityData.write('}\n\n')
 
-  abilityData.write('}\n')
+    abilityData.write('addon.AbilityGroups = {\n')
+    for key, abilityGroup in reverse_lookup.items():
+      abilityData.write('  {{ {1} }},\n'.format(key, ', '.join(abilityGroup) ));
+
+    abilityData.write('}\n')
